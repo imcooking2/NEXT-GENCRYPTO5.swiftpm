0d1ac693f86bfa652f2189d9766812e2dccb378e
Amaar <akazmi4204@stu.d214.org>
2024-04-25 12:59:49 -0500

    made the btc symbol

--- TopMoversItemView.swift
+++ TopMoversItemView.swift
@@ -5,4 +5,17 @@
 //  Created by Tomasz on 4/25/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TopMoversItemView: View{
+    var body: some View{
+        VStack(alignment: .leading){
+            
+            Image(systemName: "bitcoinsign.circle.fill")
+                .resizable()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+                .padding(.bottom, 8)
+        }
+    }
+}
