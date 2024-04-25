e7e63e92ef56380ff322d6538c17085f1a1c33a6
Tomasz <tzuber4496@stu.d214.org>
2024-04-25 13:07:55 -0500

    alt coin section m

--- altCoinCategory.swift
+++ altCoinCategory.swift
@@ -5,4 +5,38 @@
 //  Created by Tomasz on 4/25/24.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct altCoinCategory: View{
+    
+    var body:some View{
+        VStack(alignment: .leading){
+            Text("Alt Coin")
+                .font(.headline)
+                .padding()
+        }
+        HStack{
+            Text("Your coin...")
+            
+            Spacer()
+            
+            Text("Value")
+        }
+        .font(.caption)
+        .foregroundColor(.green)
+        .padding(.horizontal)
+        
+        ScrollView{
+            
+           
+            }
+        }
+    }
+
+
+struct altCoinCategory_Previews: PreviewProvider{
+    static var previews: some View{
+        altCoinCategory()
+    }
+}
