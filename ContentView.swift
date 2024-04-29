f0a4136fe71279df8c0baaba9f3a45c90e93b39a
Bryant Lertpradist <Blertpradist4516@stu.d214.org>
2024-04-29 13:27:39 -0500

    changed  up the welcomescreen

--- ContentView.swift
+++ ContentView.swift
@@ -5,11 +5,32 @@ struct ContentView: View {
     @State var balance: Double = 0.00
     var body: some View {
         NavigationView {
-            VStack {
-              
-            }
-            .navigationBarTitle("Home",displayMode: .inline)
-        }
-    }
-}
+                   VStack {
+                       Text("Welcome to Our App")
+                           .font(.largeTitle)
+                           .fontWeight(.bold)
+                           .padding()
+                       
+                       NavigationLink(destination: AchievementBar()) {
+                           Text("Tutorial")
+                           Text("Go to Detail View")
+                               .font(.title2)
+                               .fontWeight(.semibold)
+                               .foregroundColor(.white)
+                               .padding()
+                               .background(Color.blue)
+                               .cornerRadius(40)
+                       }
+                       .padding()
+                       
+                       Spacer()
+                   }
+               
+                   }
+                   .navigationBarTitle("Home", displayMode: .inline)
+               }
+           }
+
+
+
 
