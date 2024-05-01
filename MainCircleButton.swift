bfa430364e603e217b8add58c63cca92cfd9d052
Amaar <akazmi4204@stu.d214.org>
2024-05-01 12:41:10 -0500

    main circle button icon

--- MainCircleButton.swift
+++ MainCircleButton.swift
@@ -5,4 +5,18 @@
 //  Created by Amaar Kazmi on 5/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct MainCircleButton: View {
+    let iconName: String
+    
+    var body: some View{
+        
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundColor(.green)
+            .frame(width: 50, height: 50)
+            .background(Circle()
+                .foregroundColor(Color.blue))
+    }
+}
