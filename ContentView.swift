97a8b5c8ceebf702b8f3ae856865a9f0739a8c91
Bryant Lertpradist <Blertpradist4516@stu.d214.org>
2024-05-01 12:52:17 -0500

    I made navigation link for achievements

--- ContentView.swift
+++ ContentView.swift
@@ -1,46 +1,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Binding var progress: Double
+    @State var progress: Double = 0.0
     @State var balance: Double = 0.00
+    
     var body: some View {
         NavigationView {
-<<<<<<< HEAD
-                   VStack {
-                       Text("Welcome to Our App")
-                           .font(.largeTitle)
-                           .fontWeight(.bold)
-                           .padding()
-                       
-                       NavigationLink(destination: AchievementBar()) {
-                           Text("Tutorial")
-                           Text("Go to Detail View")
-                               .font(.title2)
-                               .fontWeight(.semibold)
-                               .foregroundColor(.white)
-                               .padding()
-                               .background(Color.blue)
-                               .cornerRadius(40)
-                       }
-                       .padding()
-                       
-                       Spacer()
-                   }
-               
-                   }
-                   .navigationBarTitle("Home", displayMode: .inline)
-               }
-           }
-
-
-
-=======
             VStack {
+                Text("Welcome to Our App")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
                 
+                NavigationLink(destination: AchievementBarView(progress: $progress)) {
+                    
+                    Text("Go Achievements")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(40)
+                }
+                .padding()
+                
+                Spacer()
             }
-            .navigationBarTitle("Home",displayMode: .inline)
+            .navigationBarTitle("Home", displayMode: .inline)
         }
     }
 }
->>>>>>> main
-
