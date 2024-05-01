8b14cbaa688b4aaec71de6de3fd5805e7e3d831c
Tomasz <tzuber4496@stu.d214.org>
2024-05-01 12:26:23 -0500

    madea navigation bar titled home

--- ContentView.swift
+++ ContentView.swift
@@ -3,13 +3,38 @@ import SwiftUI
 struct ContentView: View {
     @Binding var progress: Double
     @State var balance: Double = 0.00
+    @State var animatedGradient = false
     var body: some View {
         NavigationView {
-            VStack {
-                
-            }
-            .navigationBarTitle("Home",displayMode: .inline)
-        }
+                  
+                  ScrollView(.vertical, showsIndicators: false) {
+                      
+                      //movers view
+                      MoversViewTop()
+                      
+                      Divider()
+                   .overlay(
+                  LinearGradient(gradient: Gradient(colors: [.green, .black]), startPoint: .top, endPoint: .bottom)
+              )
+                      
+                      //coins view
+                      altCoinCategory()
+                      
+                      //calculations
+                      
+                      
+                         
+                  }
+               
+                  
+              }
+              
+              
+              
+          }
+              
+      }
+
     }
 }
 
