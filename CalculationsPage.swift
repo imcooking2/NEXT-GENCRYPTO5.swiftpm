f7c3cbf4f69241f5802cf8d171049c41112737f1
Kevin Kishore <kkishore6198@stu.d214.org>
2024-05-07 12:49:27 -0500

    Created calc view

--- CalculationsPage.swift
+++ CalculationsPage.swift
@@ -5,4 +5,25 @@
 //  Created by Kevin Kishore on 5/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    @State private var currentcalc = ""
+    @State private var desiredcalc = ""
+    @State private var CalcWeight = ""
+    @State private var result: Double = 0.0
+    
+    var body: some View {
+        ZStack {
+            Color(result <= 100 ? .green : .red)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Stock Caculation")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding()
+                TextField("Current Stock Value", text: $currentcalc)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.decimalPad)
+                    .padding()
+                
