7b3c2b13e8f85b6ee59c23b1f8f9a0d089b633cf
Tomasz <tzuber4496@stu.d214.org>
2024-05-07 12:39:22 -0500

    circle button view for logos

--- CircleButtonView.swift
+++ CircleButtonView.swift
@@ -5,4 +5,23 @@
 //  Created by Tomasz on 5/7/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct CircleButtonView: View{
+    let iconName: String
+    var body: some View{
+        Image(systemName: iconName)
+            .font(.headline)
+            .foregroundColor(.green)
+            .background(
+                Circle()
+                    .foregroundColor(.white)
+            )
+            .shadow(color: .blue, radius:10, x:0, y:0)
+            .frame(width:100, height: 100)
+            .padding()
+    }
+        
+}
+
+
