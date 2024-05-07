b6ee251c4e4210afa301f88d8f3c240be9ff0b8a
Amaar <akazmi4204@stu.d214.org>
2024-05-07 12:51:10 -0500

    made chart layer

--- ChartLayer.swift
+++ ChartLayer.swift
@@ -11,10 +11,13 @@ import Charts
 struct ChartLayer: View {
     
     let ydata = [100,200,300,400,500]
-    
+    let xdata = [1,2,3,4,5]
+    let data = [xdata, ydata]
     var body: some View {
      
-       
+        Chart(data) {
+            LineMark(x: .value("Months", $0), y: .value("Dollars in Hundredths", $1<#_#>))
+        }
         
     }
 }
