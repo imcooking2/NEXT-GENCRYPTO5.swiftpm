8909a8facc2b091a7bb388a847fe7eac0bb852dd
Bryant Lertpradist <Blertpradist4516@stu.d214.org>
2024-05-07 12:40:52 -0500

    Create navigation links and fix some lines of code

--- ContentView.swift
+++ ContentView.swift
@@ -17,24 +17,37 @@ struct ContentView: View {
             
             VStack {
                 Text("Welcome to Our App")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding()
-                
-                NavigationLink(destination: AchievementBarView(progress: $progress)) {
-                    Text("Tutorial")
-                    Text("Go to Detail View")
-                        .font(.title2)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(40)
-                    
-                    
-                   
-                }
-                
+                                  .font(.largeTitle)
+                                  .fontWeight(.bold)
+                                  .padding()
+                              
+                              NavigationLink(destination: AchievementBarView(progress: $progress)) {
+                                  Text("Go Achievements")
+                                      .font(.title2)
+                                      .fontWeight(.semibold)
+                                      .foregroundColor(.white)
+                                      .padding()
+                                      .background(Color.blue)
+                                      .cornerRadius(40)
+                              }
+                              
+                              NavigationLink(destination: BankAccountView(balance: $balance)) {
+                                  Text("Bank Balance")
+                                      .font(.title2)
+                                      .fontWeight(.semibold)
+                                      .foregroundColor(.white)
+                                      .padding()
+                                      .background(Color.blue)
+                                      .cornerRadius(40)
+                              }
+                              .padding()
+                              
+                              Spacer()
+                          }
+                          .navigationBarTitle("Home", displayMode: .inline)
+                      }
+                  }
+              }
                 // VStack{
                 // homeHeader
                 //HomeStatView(showPortfolio: $showPortfolio)
@@ -90,7 +103,7 @@ struct ContentView: View {
                 
                 
                 
-            }
-        }
-    }
-}
+            
+        
+    
+
