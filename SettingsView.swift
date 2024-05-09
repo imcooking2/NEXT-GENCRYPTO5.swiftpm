1685d81bcd1974be51ac03e6d64b16d35821f8fa
Amaar <akazmi4204@stu.d214.org>
2024-05-09 12:59:32 -0500

    refining the main + settings view

--- SettingsView.swift
+++ SettingsView.swift
@@ -7,26 +7,9 @@
 
 import SwiftUI
 
-struct SettingsView: View{
-    
-    let defaultURL = URL(string: "https://www.google.com")
-    let youtubeURL = URL(string: "https://www.youtube.com")
-    let jhhsWebsiteURL = URL(string: "https://www.d214.org/jhhs")
-    let coingeckoURL = URL(string: "https://www.coingecko.com")
-    
-    
-    var body: some View{
-        
-        Text("Setting Page")
-        .font(.headline)
-        .accentColor(.green)
-        .listStyle(GroupedListStyle())
-        .navigationTitle("Settings")
-        .toolbar{
-            ToolbarItem(placement: .navigationBarLeading){
-               
-            }
-        }
+struct SettingsView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 10)
     }
-    
 }
+
