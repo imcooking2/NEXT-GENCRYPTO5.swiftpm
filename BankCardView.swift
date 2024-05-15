9a0f72a5550e6aab86bc15f9c4ec26d0dc7fc887
Tomasz <tzuber4496@stu.d214.org>
2024-05-15 12:44:55 -0500

    working on the stack card effect

---
+++ BankCardView.swift
@@ -0,0 +1,14 @@
+//
+//  BankCardView.swift
+//  NEXT˝GENCRYPTO5
+//
+//  Created by Tomasz on 5/15/24.
+//
+
+import SwiftUI
+
+struct BankCardView: View{
+    var body: some View{
+        
+    }
+}
