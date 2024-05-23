44527b6a0b6ae76f1e9d99ffcd17b75c93f47aa1
Kevin Kishore <kkishore6198@stu.d214.org>
2024-05-23 13:15:35 -0500

    Deleted unescessary views and redevolped.

--- StartUpView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  StartUpView.swift
-//  NEXT˝GENCRYPTO5
-//
-//  Created by Kevin Kishore on 5/1/24.
-//
-
-import Foundation
-//Still working
--- rowSell.swift
+++
@@ -1,8 +0,0 @@
-//
-//  rowSell.swift
-//  NEXT˝GENCRYPTO5
-//
-//  Created by Tomasz on 4/25/24.
-//
-
-import Foundation
