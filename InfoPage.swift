9325329009039949cf53ef65152f09480a62b895
Kevin Kishore <kkishore6198@stu.d214.org>
2024-05-30 12:32:49 -0500

    Info page completed and added navigation.

--- InfoPage.swift
+++ InfoPage.swift
@@ -12,15 +12,24 @@ struct InfoPage: View {
     var body: some View {
         VStack(alignment: .leading){
             Text("Hello Welcome to NextGenCrypto05")
+                .foregroundColor(.blue)
             
             
-            Text("This app is about tracking cryptocurrencies.")
+            Text("This app is about tracking cryptocurrencies. We made this app to demonstrate the power of Swift Ui with crypto currency tracking. Theres a main hub, achivements, BankBalance, and settings page.")
                 .foregroundColor(.white)
                 .padding()
                 .background(Color.blue)
                 .cornerRadius(20)
         }
+        NavigationLink(destination: SettingsView()) {
+            Text("Go To SettingsPage")
+                .font(.title2)
+                .fontWeight(.semibold)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .cornerRadius(40)
+        }
+        
     }
-    
-    
 }
